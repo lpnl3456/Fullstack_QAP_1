49a60c12433ab947317f13db27a24bb4fa808c68
lpnl3456 <luke.peddle@keyin.com>
2024-05-20 11:18:15 -0230

    Added Validator Example

---
+++ Validator.js
@@ -0,0 +1,44 @@
+//validate a string to make sure it is a flaot
+var validator = require('validator')
+ let demoString = '40.06';
+
+ if(validator.isFloat(demoString) === true){
+    console.log(demoString + " is a float");
+ }
+
+ else{
+    console.log(demoString + " is not a float");
+ }
+
+ //Change the value to not be a float
+ demoString = '$56';
+
+ if(validator.isFloat(demoString) === true){
+    console.log(demoString + " is a float");
+ }
+
+ else{
+    console.log(demoString + " is not a float");
+ }
+
+ //validate an email
+ demoString = 'luke@gmail.com';
+
+ if(validator.isEmail(demoString) === true){
+    console.log(demoString + " is an email");
+ }
+
+ else{
+    console.log(demoString + " is not an email");
+ }
+
+ //Change the value to not be an email
+ demoString = 'lukegmail.com';
+
+ if(validator.isEmail(demoString) === true){
+    console.log(demoString + " is an email");
+ }
+
+ else{
+    console.log(demoString + " is not an email");
+ }
